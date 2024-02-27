8c808126e2f56f3ba822815ab055dc488f92d63f
eastcoastdeveloper <58962475+eastcoastdeveloper@users.noreply.github.com>
2024-02-26 16:49:52 -0800

    1 file modified

--- src/app/date-picker/date-picker.component.ts
+++ src/app/date-picker/date-picker.component.ts
@@ -25,7 +25,7 @@ export class DatepickerComponent implements OnInit {
   ];
   years: number[] = [
     2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014,
-    2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023,
+    2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024
   ];
   currentDay: any = this.d.getDate();
   monthIndex: any = this.d.getMonth();
